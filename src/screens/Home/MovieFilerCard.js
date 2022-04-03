bc3ccecd0e21e7de1259d99cc9446d3ae4fc5920
sayedsajidali <sajidlpu1@gmail.com>
2022-04-03 23:28:00 +0530

    Added Find Movies Component

--- src/screens/Home/MovieFilerCard.js
+++ src/screens/Home/MovieFilerCard.js
@@ -6,18 +6,26 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+
 
 const styles = (theme) => ({
-close: {
-    width: theme.spacing.unit * 4,
-    height: theme.spacing.unit * 4,
-    },
-filter_title: {
-    color: theme.palette.primary.light,
-    textTransform: 'capitalize',
-    },
   card: {
-    minWidth: 275,
+      margin: '2%',
+      width: '90%',
+      padding: '10px',
+    },
+  cmpPosition: {
+    margin: theme.spacing.unit,
+    minWidth: '240px',
+    maxWidth: '240 px',
   },
   bullet: {
     display: 'inline-block',
@@ -25,42 +33,176 @@ filter_title: {
     transform: 'scale(0.8)',
   },
   title: {
-    fontSize: 14,
+    fontSize: 16,
+    color: theme.palette.primary.light,
   },
   pos: {
     marginBottom: 12,
   },
 });
 
+const genreList = [
+  'Drama',
+  'Mystery',
+  'Thriller',
+  'Horror',
+  'Historical',
+  'Romance',
+  'Action',
+  'Science Fiction',
+  'Fantasy',
+  'Comedy',
+];
+
+const artistList = [
+  {
+    id: 0,
+    first_name: 'Sajid',
+    last_name: 'Sayed',
+  },
+  {
+    id: 1,
+    first_name: 'Kareena',
+    last_name: 'Nazia',
+  },
+  {
+    id: 2,
+    first_name: 'Haseena',
+    last_name: 'Banu',
+  },
+  {
+    id: 3,
+    first_name: 'Azgar',
+    last_name: 'Ali',
+  },
+];
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
 function SimpleCard(props) {
   const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
+
+  const [GenreName, setGenreName] = React.useState([]);
+  const handleGenreChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setGenreName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+
+  const [ArtistName, setArtistName] = React.useState([]);
+  const handleArtistChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setArtistName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const [releaseStartData, setReleaseStartData] = React.useState(new Date("dd-mm-yyyy"));
+  const handleReleaseStartData = (newValue) => {
+    setReleaseStartData(newValue);
+  };
+
+  const [releaseEndData, setReleaseEndData] = React.useState(new Date("dd-mm-yyyy"));
+  const handleReleaseEndData = (newValue) => {
+    setReleaseEndData(newValue);
+  };
+  
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography  component="h2" className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+        <Typography  component="h1" className={classes.title} gutterBottom>
+          FIND MOVIES BY:
         </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+
+        {/* Movie Name Input */}
+        <FormControl className={classes.cmpPosition}>
+          <InputLabel htmlFor="my-input">Movie Name</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+        </FormControl>
+        <br/>
+
+        {/* Genre Input */}
+        <FormControl className={classes.cmpPosition}>
+          <InputLabel htmlFor="demo-multiple-checkbox" id="demo-multiple-checkbox-label">Genre</InputLabel>
+          <Select
+            id="demo-multiple-checkbox"
+            multiple
+            value={GenreName}
+            onChange={handleGenreChange}
+            renderValue={(selected) => selected.join(', ')}
+            MenuProps={MenuProps}
+          >
+            {genreList.map((genre) => (
+              <MenuItem key={genre} value={genre}>
+                <Checkbox checked={GenreName.indexOf(genre) > -1} />
+                <ListItemText primary={genre} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <br/>
+
+        {/* Artists Name */}
+        <FormControl className={classes.cmpPosition}>
+          <InputLabel htmlFor="demo-multiple-checkbox-2" id="demo-multiple-checkbox-label-2">Artists</InputLabel>
+          <Select
+            id="demo-multiple-checkbox-2"
+            multiple
+            value={ArtistName}
+            onChange={handleArtistChange}
+            renderValue={(selected) => selected.join(', ')}
+            MenuProps={MenuProps}
+          >
+            {artistList.map((artist) => (
+              <MenuItem key={artist.id} value={`${artist.first_name} ${artist.last_name}`}>
+                <Checkbox checked={ArtistName.indexOf(artist) > -1} />
+                <ListItemText primary={`${artist.first_name} ${artist.last_name}`} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <br/>
+
+        {/* Release Start Date */}
+        <TextField  className={classes.cmpPosition}
+          name=" Release Date Start"
+          label=" Release Date Start"
+          InputLabelProps={{ shrink: true}}
+          type="date"
+          defaultValue={releaseStartData}
+      />
+
+        {/* Release End Date */}
+        <TextField  className={classes.cmpPosition}
+          name="Release Date End"
+          label="Release Date End "
+          InputLabelProps={{ shrink: true}}
+          type="date"
+          defaultValue={releaseEndData}
+      />
+      <br/>
+
+      {/* Apply Filter Button */}
+      <Button  className={classes.cmpPosition} variant="contained" color="primary">APPLY</Button>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
